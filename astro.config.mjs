6b3291f9998f569638021a0a440e4f473da6581b
Tomas Domingo <59489838+ExtraSonoro9@users.noreply.github.com>
2025-06-20 14:32:16 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://extrasonoro9.github.io/vortice/',
+	base: 'main',
 	integrations: [mdx(), sitemap()],
 });
